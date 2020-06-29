e96b24beb5e862048f2daf60226ef2e96a532cda
Nwovu Sunday <honesty1ofa1i@gmail.com>
2020-06-29 22:56:34 +0100

    changed it

--- mysite/settings.py
+++ mysite/settings.py
@@ -181,7 +181,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey' 
-EMAIL_HOST_PASSWORD = get_env_variable('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'default')
 
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
